82faba483fbe052d1d22aaeb675b69b59e99d829
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-02-15 20:52:36 -0300

    Exercicio Resolvido JAVA

--- Java/CursoUdemyJava/heranca/src/application/Program.java
+++ Java/CursoUdemyJava/heranca/src/application/Program.java
@@ -1,24 +1,20 @@
 package application;
 
 import entities.Account;
-import entities.BussinessAccount;
 import entities.SavingAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingAccount(1023, "Maria", 1000.0, 0.01);
 		
-		Account acc2 = new SavingAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		x.withdraw(50.0);
+		y.withdraw(50.0);
 		
-		Account acc3 = new BussinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 		
 	}
 }
