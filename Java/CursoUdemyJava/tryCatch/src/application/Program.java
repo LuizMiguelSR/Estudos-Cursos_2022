a511f851bc00a45cd38e844db7589afd17f9c24d
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2022-03-02 00:53:03 -0300

    att

--- Java/CursoUdemyJava/tryCatch/src/application/Program.java
+++ Java/CursoUdemyJava/tryCatch/src/application/Program.java
@@ -7,6 +7,23 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		method1();
+		
+		System.out.println("End of Program");		
+
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		
+		method2();
+		
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -16,15 +33,16 @@ public class Program {
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position");
+			e.printStackTrace();
+			sc.next();
 		}
 		catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
 		
-		System.out.println("End of Program");
-		
 		sc.close();
-
+		
+		System.out.println("***METHOD2 END***");
 	}
 
 }
--- README.md
+++ README.md
@@ -74,7 +74,8 @@
   
   - Tratamento de exceções;
     - Discussão inicial sobre exceções;
-    - [Try Catch](/Java/CursoUdemyJava/tryCatch/)
+    - [Try Catch](/Java/CursoUdemyJava/tryCatch/);
+    - [Pilha de chamada de métodos (stack trace)](/Java/CursoUdemyJava/tryCatch/);
 
 &nbsp;
 
@@ -85,6 +86,7 @@
   - [Seção 00 - Recursos do Curso](/Java/DatabaseDesign/Secao00/);
   - [Seção 01 - Recursos do Curso](/Java/DatabaseDesign/Secao01/);
     - Introdução à Oracle Academy;
+    - Dados vs Informação;
 
 &nbsp;
 
@@ -97,3 +99,4 @@
   - [Introdução](/HTMLeCSS/DigitalOne_HTML/index.html/);
   - [Entendendo o que é semântica](/HTMLeCSS/DigitalOne_HTML/index.html/);
   - [Textos e links em HTML](/HTMLeCSS/DigitalOne_HTML/index.html/);
+  - [Como inserir imagem em seu site](/HTMLeCSS/DigitalOne_HTML/index.html/);
