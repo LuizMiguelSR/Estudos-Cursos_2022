77814e6f75e0a74c548e0c1758a7f9f776825d36
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-05-20 21:33:10 -0300

    Bloco try with resources

--- Java/CursoUdemyJava/bufferedFileReader/src/application/Program.java
+++ Java/CursoUdemyJava/bufferedFileReader/src/application/Program.java
@@ -9,13 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "/home/luiz/in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader (new FileReader(path))) {		
 			String line = br.readLine();
 			
 			while (line != null) {
@@ -24,18 +19,6 @@ public class Program {
 			}
 		}catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}finally {
-			try {							
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
-
 		}
 
 	}
