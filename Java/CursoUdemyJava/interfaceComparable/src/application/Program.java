b86472ea830c0e87150e77b21bc8ac852ed1b690
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-07-06 18:37:08 -0300

    Interface Comparable: Exercício 02

--- Java/CursoUdemyJava/interfaceComparable/src/application/Program.java
+++ Java/CursoUdemyJava/interfaceComparable/src/application/Program.java
@@ -7,19 +7,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import entities.Employee;
+
 public class Program {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
+		
+		List<Employee> list = new ArrayList<>();
 		String path = "C:\\temp\\in.txt";
+		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
 			}
 			Collections.sort(list);
-			for (String s : list) {
-				System.out.println(s);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
