4cea14c760c3c8988472fa373836fc69784c81df
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-07-15 19:39:57 -0300

    Listas mistas e de definição

--- Java/JavaFundamentals/Projetos/testes/src/testes/teste.java
+++ Java/JavaFundamentals/Projetos/testes/src/testes/teste.java
@@ -20,7 +20,21 @@ public class teste {
 		 * 
 		 * int x = 0; if((5.1 > 4.3 && 6.2 < 8.4) && (7.2 < 3.5 || 1.2 == 2.1 || 2.2 !=
 		 * 2.25)) { x = 3; } else { x = 4; } System.out.print(x);
+		 * 
+		 * 
+		 * int num[] = {9,8,7,6,5,4,3,2,1}; for(int i = 0; i < 9; i = i+3) {
+		 * System.out.print(num[i]); }
+		 * 
+		 * Object array = new Object[5]; int x[] = new int[10];
 		 */
+		
+		int array[][] = { { 3, 2, 1 }, { 1, 2, 3 } };
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 3; j++)
+				System.out.print(2 * array[i][j]);
+				
+			
+		
 	}
 
 }
