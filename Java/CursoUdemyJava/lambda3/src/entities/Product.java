b4e62c6d31c42670e5bad28af6f9d155528b4942
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-08-01 16:32:54 -0300

    Reference method com método não estático

--- Java/CursoUdemyJava/lambda3/src/entities/Product.java
+++ Java/CursoUdemyJava/lambda3/src/entities/Product.java
@@ -30,6 +30,10 @@ public class Product {
 		p.setPrice(p.getPrice() * 1.1);
 	}
 	
+	public void nonStaticPriceUpdate() {
+		price = price * 1.1;
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
