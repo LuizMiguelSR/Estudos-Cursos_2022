ed0e38ed28171cbff9ed40d1c84fffff7028574c
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-08-02 18:44:40 -0300

    Implementação da interface

--- Java/CursoUdemyJava/lambda4/src/application/Program.java
+++ Java/CursoUdemyJava/lambda4/src/application/Program.java
@@ -2,8 +2,10 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
@@ -15,6 +17,10 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-
+		
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
+		
 	}
 }
