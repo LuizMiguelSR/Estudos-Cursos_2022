a129ccbd21608d99042c263206a90dbe902bd33b
Luiz Miguel <luizmiguel.srodrigues@gmail.com>
2022-08-02 18:51:15 -0300

    Reference method com método não estático

--- Java/CursoUdemyJava/lambda4/src/entities/Product.java
+++ Java/CursoUdemyJava/lambda4/src/entities/Product.java
@@ -30,6 +30,10 @@ public class Product {
 		return p.getName().toUpperCase();
 	}
 	
+	public String nonStaticUpperCaseName() {
+		return name.toUpperCase();
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
